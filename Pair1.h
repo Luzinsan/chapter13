c81ba85a7bcd4704852d08e8e57a3e26a68c744d
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2022-01-30 15:08:13 +0700

    Update Pair1.h

--- Pair1.h
+++ Pair1.h
@@ -6,11 +6,11 @@ class Pair1
 private:
 	T m_1, m_2;
 public:
-	Pair1(const T& value1 = 0, const T& value2 = 0) : m_1(value1), m_2(value2) {}
-	~Pair1() {}
+	Pair1(const T& value1 = 0, const T& value2 = 0);
+	~Pair1();
 
-	T& first() { return m_1; }
-	T& second() { return m_2; }
-	const T& first() const  { return m_1;  }
-	const T& second() const { return m_2; }
+	T& first();
+	T& second();
+	const T& first() const;
+	const T& second() const;
 };
