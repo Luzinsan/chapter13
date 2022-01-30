d004974d94e45201b50139e5b112de3de3038b25
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2022-01-30 15:07:43 +0700

    Update Pair.h

--- Pair.h
+++ Pair.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PAIR_H
+#define PAIR_H
 
 template <class T, class S>
 class Pair
@@ -7,11 +8,13 @@ private:
 	T m_1;
 	S m_2;
 public:
-	Pair(const T& value1 = 0, const S& value2 = 0) : m_1(value1), m_2(value2) {}
-	virtual ~Pair() {}
+	Pair(const T& value1 = 0, const S& value2 = 0);
+	virtual ~Pair();
 
-	T& first() { return m_1; }
-	S& second() { return m_2; }
-	const T& first() const { return m_1; }
-	const S& second() const { return m_2; }
+	T& first();
+	S& second();
+	const T& first() const;
+	const S& second() const;
 };
+
+#endif 
