0b445e47e73c69012a351b2f69ac4d2a7ccffd71
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2022-01-30 14:41:07 +0700

    Create StringValuePair.h

---
+++ StringValuePair.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Pair.h"
+#include <string>
+template <class S>
+class StringValuePair: public Pair<std::string, S>
+{
+public:
+	StringValuePair(const std::string& value1 = 0, const S& value2 = 0) : Pair<std::string, S>(value1, value2) {}
+};
