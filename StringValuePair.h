3026e4dc016611227009ad81e07b2c5aa741beb7
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2022-01-30 15:08:48 +0700

    Update StringValuePair.h

--- StringValuePair.h
+++ StringValuePair.h
@@ -5,5 +5,5 @@ template <class S>
 class StringValuePair: public Pair<std::string, S>
 {
 public:
-	StringValuePair(const std::string& value1 = 0, const S& value2 = 0) : Pair<std::string, S>(value1, value2) {}
+	StringValuePair(const std::string& value1 = 0, const S& value2 = 0);
 };
